cdc86b477ca517d13a276e51ebd83c0d3ee20e6c
x-ror <16686943+x-ror@users.noreply.github.com>
2026-03-12 16:24:12 +0000

    fix: update action_text-trix to version 2.1.17 and remove avatar_url from users table

--- db/schema.rb
+++ db/schema.rb
@@ -102,7 +102,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_12_110000) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "avatar_url"
     t.text "bio"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
